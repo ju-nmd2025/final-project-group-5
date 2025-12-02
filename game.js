d164cbe4ce2b28ccc693ba104625637d8ad56600
Setdar04 <dase25xd@student.ju.se>
2025-12-02 12:45:55 +0100

    fixed the issue with the color

--- game.js
+++ game.js
@@ -5,10 +5,6 @@ function setup() {
 let character = new Character(50, 50, 50, 50);
 
 function draw() {
-<<<<<<< Updated upstream
   background(125, 207, 182);
-=======
-  background(51, 202, 127);
   character.draw();
->>>>>>> Stashed changes
 }
