355f180a27dddd893a4b852fc4bc6a3900dc0416
Neha Rajesh <rane25jx@student.ju.se>
2025-12-12 11:46:12 +0100

    linus feedback post merge

--- game.js
+++ game.js
@@ -2,72 +2,73 @@ import { Character } from "./character.js";
 import { Platform } from "./platform.js";
 import { Spike } from "./spike.js";
 import {
-    runGame,
-    end,
-    start,
-    loadImages,
-    froggie,
-    drawButton,
-    canvasWidth,
-    canvasHeight,
-    floor,
+  runGame,
+  end,
+  start,
+  loadImages,
+  froggie,
+  drawButton,
+  canvasWidth,
+  canvasHeight,
+  floor,
 } from "./gameHandler.js";
 
 let character;
 
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
-    loadImages();
-    character = new Character(25, 450, 50, 50, froggie);
+  createCanvas(canvasWidth, canvasHeight);
+  loadImages();
+  character = new Character(25, 450, 50, 50, froggie);
 }
 
 let platforms = [
-    new Platform(150, 450, 80, 20), // near floor
-    new Platform(120, 200, 80, 20),
-    new Platform(300, 150, 80, 20),
-    new Platform(210, 0, 80, 20),
-    new Platform(310, -160, 80, 20),
-    new Platform(50, -240, 80, 20),
-    new Platform(100, -420, 80, 20),
-    new Platform(320, -500, 80, 20),
-    new Platform(240, -670, 80, 20),
-    new Platform(140, -760, 80, 20),
-    new Platform(90, -940, 80, 20),
+  new Platform(150, 450, 80, 20), // near floor
+  new Platform(120, 200, 80, 20),
+  new Platform(300, 150, 80, 20),
+  new Platform(210, 0, 80, 20),
+  new Platform(310, -160, 80, 20),
+  new Platform(50, -240, 80, 20),
+  new Platform(100, -420, 80, 20),
+  new Platform(320, -500, 80, 20),
+  new Platform(240, -670, 80, 20),
+  new Platform(140, -760, 80, 20),
+  new Platform(90, -940, 80, 20),
 ];
 
 let spikes = [
-    new Spike(60, 350, 80, 20),
-    new Spike(260, 300, 80, 20),
-    new Spike(40, 70, 80, 20),
-    new Spike(130, -80, 80, 20),
-    new Spike(260, -330, 80, 20),
-    new Spike(80, -580, 80, 20),
-    new Spike(300, -850, 80, 20),
+  new Spike(60, 350, 80, 20),
+  new Spike(260, 300, 80, 20),
+  new Spike(40, 70, 80, 20),
+  new Spike(130, -80, 80, 20),
+  new Spike(260, -330, 80, 20),
+  new Spike(80, -580, 80, 20),
+  new Spike(300, -850, 80, 20),
 ];
 
 function mousePressed() {
-    if (mouseX > 150 && mouseX < 350 && mouseY > 130 && mouseY < 230) {
-        character.gameState = "runGame";
-        character.y = floor - character.h;
-        character.landing = 0;
-        for (const platform of platforms) {
-            platform.visited = false;
-        }
+  if (mouseX > 150 && mouseX < 350 && mouseY > 130 && mouseY < 230) {
+    character.gameState = "runGame";
+    character.y = floor - character.h;
+    character.landing = 0;
+    for (const platform of platforms) {
+      platform.visited = false;
     }
+  }
 }
 
 function draw() {
-    if (character.gameState == "start") {
-        start();
-    }
+  if (character.gameState == "start") {
+    start();
+  }
 
-    if (character.gameState == "runGame") {
-        runGame(character, spikes, platforms, floor);
-    }
+  if (character.gameState == "runGame") {
+    runGame(character, spikes, platforms, floor);
+  }
 
-    if (character.gameState == "end") {
-        end(character);
-    }
+  if (character.gameState == "end") {
+    end(character);
+    drawButton(150, 130, 200, 100, 20);
+  }
 }
 
 // All your other code is above!
@@ -76,9 +77,9 @@ window.setup = setup;
 window.draw = draw;
 
 window.addEventListener("keydown", function (event) {
-    character.jump();
+  character.jump();
 });
 
 window.addEventListener("click", function (event) {
-    mousePressed();
+  mousePressed();
 });
